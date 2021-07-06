82ad3b3bd935926c0525c8bcf07ab6bc208128c7
Mayor2305 <jmayor@sfu.ca>
2021-07-06 08:58:22 -0700

    second commit

--- src/app.js
+++ src/app.js
@@ -10,15 +10,6 @@ import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from 
 import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', createdAt: 2500, amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gass bill', createdAt: 10000, amount: 1  }));
-store.dispatch(addExpense({ description: 'Rent',createdAt: 1500, amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
